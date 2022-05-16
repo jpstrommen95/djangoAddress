501f68967086a49d8d4eb66a8fb934e98cc7631b
jpstrommen95 <55213152+jpstrommen95@users.noreply.github.com>
2022-05-16 12:13:39 -0600

    enable editing of basic info

--- mysite/address/urls.py
+++ mysite/address/urls.py
@@ -2,14 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'address'  # give it a namespace here
+
 # create the url path here
 urlpatterns = [
     path('', views.index, name='login'),
     path('home/', views.home, name='home'),
-    # ex: /polls/5/
     path('<int:contact_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:contact_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:contact_id>/vote/', views.vote, name='vote'),
+    path('<int:contact_id>/edit/', views.edit, name='edit'),
 ]
--- mysite/address/views.py
+++ mysite/address/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Contact
 
 
@@ -21,10 +23,23 @@ def detail(request, contact_id):
     return render(request, 'address/detail.html', {'contact': contact})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def edit(request, contact_id):
+    # get contact
+    old_contact = get_object_or_404(Contact, pk=contact_id)
+    # edit contact
+    try:
+        old_contact.first_name = request.POST['fName']
+        old_contact.last_name = request.POST['lName']
+        old_contact.email_address = request.POST['email']
+        old_contact.street_address = request.POST['street']
+        old_contact.save()
+    except (KeyError, Contact.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'address/detail.html', {
+            'contact': old_contact,
+            'error_message': "Field does not exist, report this bug to an admin.",
+        })
+    # delete phone numbers todo
 
+    return HttpResponseRedirect(reverse('address:home'))
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
